57932ae62dfa458996e94382b4054504a26f04e6
KingCosmic <KingCosmicDev@gmail.com>
2021-09-28 11:47:16 -0500

    moved things over to regex

--- build/index.js
+++ build/index.js
@@ -4,17 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const words_json_1 = __importDefault(require("./words.json"));
-const charmap_json_1 = __importDefault(require("./charmap.json"));
 class ProfanityFilter {
     static isProfane(content) {
         const words = content.split(' ');
         for (let w = 0; w < words.length; w++) {
-            let word = words[w];
-            if (this.badwords.includes(word)) {
-                return true;
-            }
-            else if (this.containsVariant(word)) {
-                return true;
+            for (let bw = 0; bw < this.badwords.length; bw++) {
+                if (words[w].match(this.badwords[bw])) {
+                    return true;
+                }
             }
         }
         return false;
@@ -22,25 +19,40 @@ class ProfanityFilter {
     static clean(content) {
         const words = content.split(' ');
         for (let w = 0; w < words.length; w++) {
-            let word = words[w];
-            if (this.badwords.includes(word)) {
-                words[w] = '*';
-            }
-            else if (this.containsVariant(word)) {
-                words[w] = '*';
+            for (let bw = 0; bw < this.badwords.length; bw++) {
+                words[w] = words[w].replace(this.badwords[bw], '**');
             }
         }
         return words.join(' ');
     }
-    static containsVariant(word) {
-        const letters = word.split('');
-        for (let l = 0; l < letters.length; l++) {
-            const variant = this.numberToLetter[letters[l]];
-            letters[l] = (variant) ? variant : letters[l];
-        }
-        return this.badwords.includes(letters.join(''));
-    }
 }
-ProfanityFilter.badwords = words_json_1.default;
-ProfanityFilter.numberToLetter = charmap_json_1.default;
+ProfanityFilter.badwords = words_json_1.default.map(word => {
+    word = word.replace(/a/g, '[a4@]+');
+    word = word.replace(/b/g, '[b8]+');
+    word = word.replace(/c/g, '[c<]+');
+    word = word.replace(/d/g, '[d]+');
+    word = word.replace(/e/g, '[e3]+');
+    word = word.replace(/f/g, '[f]+');
+    word = word.replace(/g/g, '[g]+');
+    word = word.replace(/h/g, '[h]+');
+    word = word.replace(/i/g, '[i]+');
+    word = word.replace(/j/g, '[j]+');
+    word = word.replace(/k/g, '[k]+');
+    word = word.replace(/l/g, '[l]+');
+    word = word.replace(/m/g, '[m]+');
+    word = word.replace(/n/g, '[n]+');
+    word = word.replace(/o/g, '[o0]+');
+    word = word.replace(/p/g, '[p]+');
+    word = word.replace(/q/g, '[q]+');
+    word = word.replace(/r/g, '[r]+');
+    word = word.replace(/s/g, '[s5\\$]+');
+    word = word.replace(/t/g, '[t7\\+]+');
+    word = word.replace(/u/g, '[u]+');
+    word = word.replace(/v/g, '[v]+');
+    word = word.replace(/w/g, '[w]+');
+    word = word.replace(/x/g, '[x]+');
+    word = word.replace(/y/g, '[y]+');
+    word = word.replace(/z/g, '[z]+');
+    return new RegExp(`(${word})`, 'ig');
+});
 exports.default = ProfanityFilter;
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,41 @@
 import badWords from './words.json'
-import charmap from './charmap.json'
-
-// TODO: look into condensing words to check for bad words
-// like we say "boob" is a bad word but they type "boooobs"
-// instead of having several longer versions of "boob" we condense
-// maching characters together to 1 and 2 versions and check that
-// against our words.
-
-// TODO: add support for 2 word profanities
-// cock sucker will clean `cock` but leave sucker atm.
 
 // TODO: add support for seperating words "f u c k e r"
 
 class ProfanityFilter {
   // set our default bad words.
-  static badwords:string[] = badWords
+  static badwords:RegExp[] = badWords.map(word => {
+    word = word.replace(/a/g, '[a4@]+')
+    word = word.replace(/b/g, '[b8]+')
+    word = word.replace(/c/g, '[c<]+')
+    word = word.replace(/d/g, '[d]+')
+    word = word.replace(/e/g, '[e3]+')
+    word = word.replace(/f/g, '[f]+')
+    word = word.replace(/g/g, '[g]+')
+    word = word.replace(/h/g, '[h]+')
+    word = word.replace(/i/g, '[i]+')
+    word = word.replace(/j/g, '[j]+')
+    word = word.replace(/k/g, '[k]+')
+    word = word.replace(/l/g, '[l]+')
+    word = word.replace(/m/g, '[m]+')
+    word = word.replace(/n/g, '[n]+')
+    word = word.replace(/o/g, '[o0]+')
+    word = word.replace(/p/g, '[p]+')
+    word = word.replace(/q/g, '[q]+')
+    word = word.replace(/r/g, '[r]+')
+    word = word.replace(/s/g, '[s5\\$]+')
+    word = word.replace(/t/g, '[t7\\+]+')
+    word = word.replace(/u/g, '[u]+')
+    word = word.replace(/v/g, '[v]+')
+    word = word.replace(/w/g, '[w]+')
+    word = word.replace(/x/g, '[x]+')
+    word = word.replace(/y/g, '[y]+')
+    word = word.replace(/z/g, '[z]+')
 
-  static numberToLetter:{
-    [letter:string]: string
-  } = charmap
+    // word = word.replace(/]+/g, ']+[^\\w]*')
+
+    return new RegExp(`(${word})`, 'ig')
+  })
 
   /**
    * does this string contain any profane words?
@@ -27,16 +44,20 @@ class ProfanityFilter {
     // split the string by words
     const words = content.split(' ')
 
+    // loop through our sentence
     for (let w = 0; w < words.length; w++) {
-      let word = words[w]
 
-      if (this.badwords.includes(word)) {
-        return true
-      } else if (this.containsVariant(word)) {
-        return true
+      // loop through our badwords
+      for (let bw = 0; bw < this.badwords.length; bw++) {
+
+        // is this the badword that matches here?
+        if (words[w].match(this.badwords[bw])) {
+          return true
+        }
       }
     }
 
+    // return false since we didn't find any bad words.
     return false
   }
 
@@ -48,38 +69,17 @@ class ProfanityFilter {
     // split the string by words
     const words = content.split(' ')
 
+    // loop through our sentence
     for (let w = 0; w < words.length; w++) {
-      let word = words[w]
 
-      if (this.badwords.includes(word)) {
-        words[w] = '*'
-      } else if (this.containsVariant(word)) {
-        words[w] = '*'
+      // loop through our badwords
+      for (let bw = 0; bw < this.badwords.length; bw++) {
+        words[w] = words[w].replace(this.badwords[bw], '**')
       }
     }
 
     // return a cleaned sentence.
     return words.join(' ')
-  }
-
-  /**
-   * checks to see if a word could be a bad word if letters were to be replaced with numbers
-   */
-  static containsVariant(word:string) {
-    // split our word into letters
-    const letters = word.split('')
-
-    // loop through all our letters
-    for (let l = 0; l < letters.length; l++) {
-      // grab a variant if one exists.
-      const variant = this.numberToLetter[letters[l]]
-
-      // replace it if it does.
-      letters[l] = (variant) ? variant : letters[l]
-    }
-
-    // is this a bad word once our letters are replaced?
-    return this.badwords.includes(letters.join(''))
   }
 }
 
